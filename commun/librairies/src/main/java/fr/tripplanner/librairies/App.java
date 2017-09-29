30b8ed09899d26f05336042050b354b3f77548a0
David Lapierre <db.lapierre@gmail.com>
2017-09-29 15:53:08 +1300

    Initialisation du projet

---
+++ commun/librairies/src/main/java/fr/tripplanner/librairies/App.java
@@ -0,0 +1,13 @@
+package fr.tripplanner.librairies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/fr/codeyourdream/tripplanner/tripplanner/parent/App.java
@@ -0,0 +1,13 @@
+package fr.codeyourdream.tripplanner.tripplanner.parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
